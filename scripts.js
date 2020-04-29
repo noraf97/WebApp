24552573f5f750025b92e31f33bfdd636f55072d
norafekeshazy <63554669+norafekeshazy@users.noreply.github.com>
2020-04-29 18:31:47 +0200

    images added, files crated

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let mosaicdata = {
+    photo: 'images/tea.jpg',
+    title: 'Antique teacup',
+    description: 'A woman is like a tea bag – you can’t tell how strong she is until you put her in hot water.-Eleanor Roosevelt',
+};
+
+$('#photo').attr('src', data.photo);
